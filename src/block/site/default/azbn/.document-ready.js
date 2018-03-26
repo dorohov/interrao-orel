d12dc1cb33c5526da36bfacb5f08c28d09a9c5b2
azbn <i@azbn.ru>
2018-03-26 10:16:52 +0300

    201803261016

---
+++ src/block/site/default/azbn/.document-ready.js
@@ -0,0 +1,11 @@
+(function(){
+	
+	$(document.body).on('click.azbn7', '.azbn7__phone__link', null, function(event){
+		if(screenJS.isSM()) {
+			
+		} else {
+			event.preventDefault();
+		}
+	});
+	
+})();
